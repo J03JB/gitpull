3e327efde72c16ebcc1e887fcf76def44511c2f7
Frees3c <joeyjordanbarnes@hotmail.com>
2023-04-05 21:46:00 +0100

    added `--add` option

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,38 @@
-use std::env;
-use std::fs::File;
+// TODO: add_repo(): if inside git dir, add it to list of repos.
+use clap::Parser;
+use std::fs::{File, OpenOptions};
+use std::io::Write;
 use std::io::{BufRead, BufReader};
 use std::process::Command;
 
+#[derive(Parser, Debug)]
+#[clap(version = "0.1", author = "Frees3c")]
+struct Opts {
+    #[clap(short, long, value_name = "add directory to list")]
+    add: Option<String>,
+    #[clap(short, long, value_name = "directory to remove from list")]
+    delete: Option<String>,
+}
+
 fn main() {
+    let opts = Opts::parse();
+
+    // add repository to list
+    if let Some(repo) = opts.add {
+        add_repo(repo);
+        return;
+    }
+
     let repos = File::open("gr.txt").unwrap();
     let repo = BufReader::new(repos);
 
+    // pull repos listed in gr.txt
     for line in repo.lines() {
         let gitrepo = line.unwrap();
         gitpull(&gitrepo);
     }
 }
 
-// TODO: make function to add repo to gr.txt via --add.
 // TODO: make function to remove repo from list --remove
 
 fn gitpull(gitrepo: &str) {
@@ -29,4 +48,14 @@ fn gitpull(gitrepo: &str) {
         // print output to console using stdout
         println!("Git: {}", String::from_utf8_lossy(&output.stdout));
     }
+}
+
+fn add_repo(repo: String) {
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open("gr.txt")
+        .expect("failed to open input file");
+    writeln!(file, "{}", repo).expect("failed to write to input file");
+    println!("Added '{}' to input file", repo);
 }
