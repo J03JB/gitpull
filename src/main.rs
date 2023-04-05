bf11b0f9d225159c0c6e70044d6a966473b605af
Frees3c <joeyjordanbarnes@hotmail.com>
2023-04-05 22:15:26 +0100

    added `del_repo` function

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,10 @@ fn main() {
         add_repo(repo);
         return;
     }
+    if let Some(repo) = opts.delete {
+        del_repo(repo);
+        return;
+    }
 
     let repos = File::open("gr.txt").unwrap();
     let repo = BufReader::new(repos);
@@ -32,8 +36,6 @@ fn main() {
         gitpull(&gitrepo);
     }
 }
-
-// TODO: make function to remove repo from list --remove
 
 fn gitpull(gitrepo: &str) {
     let mut command = Command::new("git");
@@ -56,6 +58,48 @@ fn add_repo(repo: String) {
         .append(true)
         .open("gr.txt")
         .expect("failed to open input file");
+    let repo = if repo == "." {
+        let cwd = std::env::current_dir().unwrap();
+        cwd.to_str().unwrap().to_owned()
+    } else {
+        repo
+    };
     writeln!(file, "{}", repo).expect("failed to write to input file");
     println!("Added '{}' to input file", repo);
+}
+
+// TODO: make "." possible, same as above.
+fn del_repo(repo: String) {
+    let input_file = "gr.txt";
+    let temp_file = "temp.txt";
+
+    let mut input = File::open(input_file).expect("failed to open input file");
+    let mut temp = File::create(temp_file).expect("failed to create temp file");
+
+    let mut found = false;
+    let mut line = String::new();
+
+    // read input file line by line
+    let mut reader = BufReader::new(input);
+    while reader.read_line(&mut line).unwrap() > 0 {
+        // check if the line contains the repository to delete
+        if line.trim() == repo {
+            found = true;
+        } else {
+            // write the line to the temp file if it's not the repository to delete
+            temp.write(line.as_bytes())
+                .expect("failed to write to temp file");
+        }
+        line.clear();
+    }
+
+    if found {
+        // replace the input file with the temp file
+        std::fs::rename(temp_file, input_file).expect("failed to replace input file");
+        println!("Removed '{}' from input file", repo);
+    } else {
+        // delete the temp file if the repository was not found
+        std::fs::remove_file(temp_file).expect("failed to delete temp file");
+        println!("'{}' not found in input file", repo);
+    }
 }
