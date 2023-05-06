49e5e9aa1c71dee5707c8530351dd5f8f2c4cf75
Joe B <>
2023-05-06 15:51:08 +0100

    fix: nesting....

--- src/git.rs
+++ src/git.rs
@@ -16,34 +16,35 @@ pub fn git_pull(repo_name: Option<&str>) {
     for line in repos.lines() {
         let gitrepo = line.expect(&format!("Failed to read line from '{}'", GR_FILE_PATH));
         if repo_name.is_none() || gitrepo.trim().ends_with(repo_name.unwrap()) {
-            // spawn a new thread for each repository
-            let thread_me_daddy = thread::spawn(move || {
-                // println!("Pulling from {} ...\n", gitrepo);
-                let pull = Command::new("git")
-                    .arg("-C")
-                    .arg(&gitrepo)
-                    .arg("-c")
-                    .arg("color.ui=always")
-                    .arg("pull")
-                    .output()
-                    .expect("failed to execute git");
-
-                if pull.status.success() {
-                    println!("Git pull succeeded for '{}'", gitrepo);
-                    println!("Git: {}", String::from_utf8_lossy(&pull.stdout));
-                } else {
-                    eprintln!("Error: Git pull failed for '{}'", gitrepo);
-                    eprintln!("Git: {}", String::from_utf8_lossy(&pull.stderr));
-                }
-            });
-
-            // add the thread to the vcector
-            threads.push(thread_me_daddy);
+            continue;
         }
+        // spawn a new thread for each repository
+        let thread = thread::spawn(move || {
+            // println!("Pulling from {} ...\n", gitrepo);
+            let pull = Command::new("git")
+                .arg("-C")
+                .arg(&gitrepo)
+                .arg("-c")
+                .arg("color.ui=always")
+                .arg("pull")
+                .output()
+                .expect("failed to execute git");
+
+            if pull.status.success() {
+                println!("Git pull succeeded for '{}'", gitrepo);
+                println!("Git: {}", String::from_utf8_lossy(&pull.stdout));
+            } else {
+                eprintln!("Error: Git pull failed for '{}'", gitrepo);
+                eprintln!("Git: {}", String::from_utf8_lossy(&pull.stderr));
+            }
+        });
+
+        // add the thread to the vcector
+        threads.push(thread);
     }
 
     // wait for all threads to finsih
-    for thread_me_daddy in threads {
-        thread_me_daddy.join().unwrap();
+    for thread in threads {
+        thread.join().unwrap();
     }
 }
